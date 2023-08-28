f050bcf51c97af1e897ecb6c577e29927ece93b6
BullBull-B <bullbullonline@gmail.com>
2023-08-28 09:57:22 +0545

    Scrolloff added in init.lua

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,5 @@
 vim.opt.rnu = true -- set relative line numbers
 vim.opt.guicursor = "" -- set cursor in block mode
-
+vim.opt.scrolloff = 8 -- set scrolloff 8
+-- vim.opt.hlsearch = false --set search highlighting off
 
