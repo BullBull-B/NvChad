b22d822436d0b32be75af44db312d838dafa6443
BullBull-B <bullbullonline@gmail.com>
2023-09-23 22:52:58 +0545

    Install all LSPs i use. Check in lua/custom/configs/lspconfig.lua

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -2,19 +2,8 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
--- mason ensure install
-lspconfig.mason.setup{
-  ensure_installed = {"clangd",
-    "css-lsp",
-    "emmet-language-server",
-    "html-lsp",
-    "intelephense",
-    "jdtls",
-    "lemminx",
-    "lua-language-server",
-    "prettierd",
-    "xmlformatter"}
-}
+--you can copy and install these LSPs by pasting below line in command mode:
+--MasonInstall clangd css-lsp emmet-language-server html-lsp intelephense jdtls lemminx luae-language-server prettierd xmlformatter
 
 -- xml setup
 lspconfig.intelephense.setup{
