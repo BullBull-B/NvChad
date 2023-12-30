f9715d0097bbd63be1d1ec44f2bf97db23a19c9d
BullBull-B <bullbullonline@gmail.com>
2023-12-30 17:06:39 +0545

    Some Changes with plugins

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -9,6 +9,20 @@ M.general = {
   }
 }
 
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    }
+  }
+}
+
 -- Mappings Added by BullBull
 
 -- To wrap line below remaining where you are
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,101 @@
 local plugins = {
   {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "clangd",
+        "codelldb",
+        "clang-format",
+        "css-lsp",
+        "html-lsp",
+        "lua-language-server",
+        "emmet-language-server",
+        "prettierd",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+      }
+    }
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "php",
+      "html",
+    },
+    config = function ()
+      require("nvim-ts-autotag").setup()
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function ()
+      opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "go",
+        "php",
+        "html",
+        "css",
+        "vim",
+        "c",
+        "cpp",
+        "rust",
+      }
+      return opts
+    end
+  },
+  {
     "aurum77/live-server.nvim",
     cmd = {
       "LiveServer",
@@ -14,7 +110,7 @@ local plugins = {
 
   {
     "vimwiki/vimwiki",
-    event = "VimEnter",
+    event = "VeryLazy",
   },
 
   {
